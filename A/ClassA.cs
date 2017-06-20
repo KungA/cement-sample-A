4721874a8c00fb83987fd1b0763dc296249a2097
kungurtsev <kungurtsev@skbkontur.ru>
2017-06-20 13:43:56 +0500

    init

---
+++ A/ClassA.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using B;
+using C;
+
+namespace A
+{
+    public static class ClassA
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, this is A");
+            Console.WriteLine("It also use:");
+            new ClassB().Run();
+            new ClassC().Run();
+        }
+    }
+}
